46bb47a4ae6792f0a0b52d3226dc3ad69c4861a8
Pichai Sivawat <tingtong003tomy@gmail.com>
2013-04-21 18:06:57 +0700

    Add support EXIF

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href='http://fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-	<script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.core.js"></script>
-	<script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.slide.js"></script>
+	<script type="text/javascript" src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.core.js"></script>
+	<script type="text/javascript" src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.slide.js"></script>
+	<script type="text/javascript" src="jquery.exif.js"></script>
 </head>
 <body>
 	<?php
 	$file_count = array();
-   	for($i=1; $i<=6; $i++) { //$i = number of folder
+   	for($i=1; $i<=4; $i++) { //$i = number of folder
    		$dir = "alb" . $i;
    		$file_count[$i] = count(glob("{$dir}/*.jpg")); //count img in folder
    	}
@@ -20,14 +21,14 @@
 
 
 	<div class="albumthumb" id="1">
-		<img class="imgalbumthumb" src="alb1/img4.jpg">
-		<p>Album1</p>
+		<img class="imgalbumthumb" src="alb1/img15.jpg">
+		<p>Nature</p>
 	</div>
 	<div id="album1">
 		<div id="thumbcontainer">
 			<?php
 				for($j = 1; $j <= $file_count[1]; $j++) {
-    				echo "<img class='thumb' src='alb1/img" .  $j . ".jpg'>";
+    				echo "<img class='thumb' exif='true' src='alb1/img" .  $j . ".jpg'>";
     			}
     		?>
     		<div id="vr"></div>
@@ -35,14 +36,14 @@
 	</div> 
 
 	<div class="albumthumb" id="2">
-		<img class="imgalbumthumb" src="alb1/img5.jpg">
-		<p>Album2</p>
+		<img class="imgalbumthumb" src="alb2/img5.jpg">
+		<p>Landscape</p>
 	</div>
 	<div id="album2">
 		<div id="thumbcontainer">
 			<?php
 				for($j = 1; $j <= $file_count[2]; $j++) {
-					echo "<img class='thumb' src='alb2/img" .  $j . ".jpg'>";
+					echo "<img class='thumb' exif='true' src='alb2/img" .  $j . ".jpg'>";
     			}
     		?>
     		<div id="vr"></div>
@@ -50,14 +51,14 @@
 	</div>
 
 	<div class="albumthumb" id="3">
-		<img class="imgalbumthumb" src="alb1/img11.jpg">
-		<p>Album3</p>
+		<img class="imgalbumthumb" src="alb3/img1.jpg">
+		<p>Miscellneous</p>
 	</div>
 	<div id="album3">
 		<div id="thumbcontainer">
 			<?php
 				for($j = 1; $j <= $file_count[3]; $j++) {
-    				echo "<img class='thumb' src='alb3/img" .  $j . ".jpg'>";
+    				echo "<img class='thumb' exif='true' src='alb3/img" .  $j . ".jpg'>";
     			}
     		?>
     	<div id="vr"></div>
@@ -65,44 +66,14 @@
 	</div>
 
 	<div class="albumthumb" id="4">
-		<img class="imgalbumthumb" src="alb1/img7.jpg">
-		<p>Album4</p>
+		<img class="imgalbumthumb" src="alb4/img14.jpg">
+		<p>Aquarium</p>
 	</div>
 	<div id="album4">
 		<div id="thumbcontainer">
 			<?php
 				for($j = 1; $j <= $file_count[4]; $j++) {
-    				echo "<img class='thumb' src='alb4/img" .  $j . ".jpg'>";
-    			}
-    		?>
-    		<div id="vr"></div>
-    	</div>
-	</div>
-
-	<div class="albumthumb" id="5">
-		<img class="imgalbumthumb" src="alb1/img3.jpg">
-		<p>Album5</p>
-	</div>
-	<div id="album5">
-		<div id="thumbcontainer">
-			<?php
-				for($j = 1; $j <= $file_count[5]; $j++) {
-    				echo "<img class='thumb' src='alb5/img" .  $j . ".jpg'>";
-    			}
-    		?>
-    		<div id="vr"></div>
-    	</div>
-	</div>
-
-	<div class="albumthumb" id="6">
-		<img class="imgalbumthumb" src="alb1/img9.jpg">
-		<p>Album6</p>
-	</div>
-	<div id="album6">
-		<div id="thumbcontainer">
-			<?php
-				for($j = 1; $j <= $file_count[6]; $j++) {
-    				echo "<img class='thumb' src='alb6/img" .  $j . ".jpg'>";
+    				echo "<img class='thumb' exif='true' src='alb4/img" .  $j . ".jpg'>";
     			}
     		?>
     		<div id="vr"></div>
@@ -135,11 +106,29 @@
 	});
 
 	$(".thumb").click( function () {
+		var model = $(this).exif('Model');
+		var exposuretime = $(this).exif('ExposureTime');
+		var fnumber = $(this).exif('FNumber');
+		var iso = $(this).exif('ISOSpeedRatings');
+		var focallenght = $(this).exif('FocalLength');
+		var software = $(this).exif('Software');
 		$("#fullcontainer").empty();
 		$(this).clone().appendTo("#fullcontainer");
-		$("<div id='exif'></div>").appendTo("#fullcontainer"); //TODO add support exif
+		$("<div id='exif'><pre class='l'>" + 
+		"Model: " + model + "</pre><pre class='r'>ExposureTime: " + exposuretime + "</pre><br><pre class='l'>" +
+		"FNumber: " + fnumber + "</pre><pre class='r'>ISO: " + iso + "</pre><br><pre class='l'>" +
+		"FocalLength: " + focallenght + "</pre><pre class='r'>Software: " + software + "</pre></div>").appendTo("#fullcontainer");
 		$("#fullcontainer").children().removeClass("thumb").addClass("full").hide();
 		$(".full").fadeIn("slow");
+	});
+
+	$("body").delegate("#exif", "mouseenter mouseleave",
+	function (event) {
+		if (event.type == 'mouseenter') {
+			$(this).fadeOut("slow");
+		} else {
+			$(this).delay(700).fadeIn("slow"); //fix double fade
+		}
 	});
 	</script>
 </body>
